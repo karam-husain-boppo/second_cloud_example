1e4001acfe0b97ed1c2aa69887e20d67e03d0186
karamuser <karamhussain354@gmail.com>
2022-05-06 15:28:09 +0530

    initial commit

---
+++ second_cloud_function/src/main/java/com/example/demo/MyConsumer.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+import java.util.function.Consumer;
+
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class MyConsumer implements Consumer<String> {
+@Override
+public void accept(String s) {
+	System.out.println("hii"+s);
+	System.out.println("ffgdgdg");
+}
+}
